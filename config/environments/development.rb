c2b2ad159382b602bccd7ca73e75d03346f922e2
Konstantin Yatsenko <moahtdeep@gmail.com>
2021-11-15 16:27:11 +0900

    Add devise initial setup

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -50,6 +50,9 @@ Rails.application.configure do
   # number of complex assets.
   config.assets.debug = false
 
+  # default url options in your environments files
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
