e45462fd06ea4aacd9a88751ae839675d39e49da
Konstantin Yatsenko <moahtdeep@gmail.com>
2021-11-16 23:03:05 +0900

    Add dependent destroy on user-item pair

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
   devise :database_authenticatable,
          :recoverable, :rememberable, :validatable
   has_many :bookings
-  has_many :items
+  has_many :items, dependent: :destroy
   has_many :bookings_as_owner, through: :items, source: :bookings
   validates :first_name, presence: true
   validates :last_name, presence: true
