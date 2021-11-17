b742555a0be0787e172c474f6948313b20d7990b
Ryan Johnson <ryan.a.y.johnson@gmail.com>
2021-11-17 12:10:24 +0900

    Add mapbox.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,13 @@
 class ItemsController < ApplicationController
   def index
     @items = policy_scope(Item)
+
+    @markers = @items.geocoded.map do |item|
+      {
+        lat: item.latitude,
+        lng: item.longitude
+      }
+    end
   end
 
   def show
