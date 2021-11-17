27be00f25e6f48d1d9b86aa708d0aac182f668ee
Ryan Johnson <ryan.a.y.johnson@gmail.com>
2021-11-17 13:46:46 +0900

    Add markers and search to map on index and show pages.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,16 +5,21 @@ class ItemsController < ApplicationController
     @markers = @items.geocoded.map do |item|
       {
         lat: item.latitude,
-        lng: item.longitude
+        lng: item.longitude,
+        info_window: render_to_string(partial: "info_window", locals: { item: item })
       }
     end
   end
+
+  # def search
+  # end
 
   def show
     @item = Item.find(params[:id])
     @booking = Booking.new
     @user = User.find(@item.user_id)
     authorize @item
+    index # Allow map to get data attributes from all Items for markers.
   end
   # We have routes for new & create but not part of the initial core journey
   # Need to add strong params for create
